fa46c5d1425d9744435a1c9484a5f399fdf4da12
CameronC17 <cameronchalmers@hotmail.co.uk>
2016-11-01 09:57:26 +0000

    basic land being drawn

--- client/js/sulran.js
+++ client/js/sulran.js
@@ -3,9 +3,7 @@ class Sulran {
     this.socket = new Connection(connString);
     this.graphics = new Draw(canvas);
 
-    this.userPosition = [10, 10];
-
-    console.log(map);
+    this.userPosition = [15, 15];
 
     this.c = document.getElementById(canvas);
     this.ctx = this.c.getContext("2d");
@@ -33,15 +31,27 @@ class Sulran {
 
   buildMap() {
     var rtnMap = [];
-    for (var y = this.userPosition[1] - 6; y < this.userPosition[1] + 6; y++) {
+    for (var y = this.userPosition[1] - 10; y < this.userPosition[1] + 10; y++) {
       var row = [];
-      for (var x = this.userPosition[0] - 8; x < this.userPosition[0]; x++) {
-
+      for (var x = this.userPosition[0] - 13; x < this.userPosition[0] + 13; x++) {
+        if (y < 0 || x < 0) {
+          row.push("b");
+        } else if (y >= map.sulran.ground.length || x >= map.sulran.ground[0].length) {
+          row.push("b");
+        } else {
+          row.push(map.sulran.ground[y][x]);
+        }
       }
+      rtnMap.push(row);
     }
+    this.graphics.visibleMap = rtnMap;
   }
 
   draw() {
+    //get all necessary data
+    this.buildMap();
+
+    //draw
     this.graphics.draw();
   }
 }
@@ -57,7 +67,7 @@ class Draw {
     this.c = document.getElementById(canvas);
     this.ctx = this.c.getContext("2d");
 
-    this.map = [];
+    this.visibleMap = [];
   }
 
   clearScreen() {
@@ -80,9 +90,44 @@ class Draw {
     this.chat();
   }
 
-  draw(map) {
+  getTile(tile) {
+    switch (tile) {
+      case "w1":
+        return "#ff0000";
+        break;
+      case "g1":
+        return "#33ff33";
+        break;
+      case "b":
+        return "#000";
+        break;
+      default:
+        return "#0000ff";
+        break;
+    }
+  }
+
+  visibleLand() {
+    var xPos = 0,
+        yPos = 0;
+    for (var y = 0; y < this.visibleMap.length; y++) {
+      for (var x = 0; x < this.visibleMap[0].length; x++) {
+        var tile = this.visibleMap[y][x];
+        this.ctx.fillStyle = this.getTile(tile);
+        this.ctx.fillRect(xPos, yPos, 40, 40);
+        xPos += 40;
+      }
+      xPos = 0;
+      yPos += 40;
+    }
+
+    //this.ctx.fillStyle="#00ff00";
+    //this.ctx.fillRect(300, 300, 300, 300)
+  }
+
+  draw() {
     this.clearScreen();
-    //this.drawMap();
+    this.visibleLand();
     this.UI();
   }
 
