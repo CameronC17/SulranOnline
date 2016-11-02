6f0019de6aecd2959c247b784721c7166657db44
CameronC17 <cameronchalmers@hotmail.co.uk>
2016-11-02 21:48:43 +0000

    client side movement working in OO methods

--- client/js/sulran.js
+++ client/js/sulran.js
@@ -1,224 +1,248 @@
 debug = true;
 
 class Sulran {
-  constructor(canvas, connString) {
-    this.connection = new Connection(connString);
-    this.graphics = new Draw(canvas);
-    this.map = new Map();
-    this.player = new Player(this.map);
+    constructor(canvas, connString) {
+        this.connection = new Connection(connString);
+        this.graphics = new Draw(canvas);
+        this.map = new Map();
+        this.player = new Player(this.map);
 
-    this.c = document.getElementById(canvas);
-    this.ctx = this.c.getContext("2d");
+        this.c = document.getElementById(canvas);
+        this.ctx = this.c.getContext("2d");
 
-    //variables
-    this.mouse = {
-      "down": [-1, -1],
-      "up": [-1, -1],
-      "move": [-1, -1]
+        //variables
+        this.mouse = {
+            "down": [-1, -1],
+            "up": [-1, -1],
+            "move": [-1, -1]
+        }
+
+        this.Key = this.createKey();
     }
 
-    this.Key = this.createKey();
-  }
-
-  mouseEvent(e, type) {
-    var pos = this.getMousePos(e);
-    this.mouse[[type]] = [pos.x, pos.y];
-  }
-
-  getMousePos(evt) {
-    var rect = this.c.getBoundingClientRect();
-    return {
-      x: evt.clientX - rect.left,
-      y: evt.clientY - rect.top
+    mouseEvent(e, type) {
+        var pos = this.getMousePos(e);
+        this.mouse[[type]] = [pos.x, pos.y];
     }
-  }
 
-  createKey() {
-    var obj = this;
-    return {
-    	//http://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
-      _pressed: {},
-      LEFT: 65,
-      UP: 87,
-      RIGHT: 68,
-      DOWN: 83,
-      isDown: function(keyCode) {
-        return this._pressed[keyCode];
-      },
-      onKeydown: function(event) {
-        var movement = {
-          "up": false,
-          "dowm": false,
-          "left": false,
-          "right": false,
+    getMousePos(evt) {
+        var rect = this.c.getBoundingClientRect();
+        return {
+            x: evt.clientX - rect.left,
+            y: evt.clientY - rect.top
         }
-        if (event.keyCode == 65) {
-          movement.left = true;
-        }
-        if (event.keyCode == 68) {
-          movement.right = true;
-        }
-        if (event.keyCode == 87) {
-          movement.up = true;
-        }
-        if (event.keyCode == 83) {
-          movement.down = true;
-        }
-
-        obj.player.move(movement);
-
-        this._pressed[event.keyCode] = true;
-      },
-      onKeyup: function(event) {
-        delete this._pressed[event.keyCode];
-      }
     }
-  }
 
-  draw() {
-    //get all necessary data
-    this.graphics.visibleMap = this.map.build(this.player.position);
+    createKey() {
+        var obj = this;
+        return {
+            //http://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
+            _pressed: {},
+            LEFT: 65,
+            UP: 87,
+            RIGHT: 68,
+            DOWN: 83,
+            isDown: function(keyCode) {
+                return this._pressed[keyCode];
+            },
+            onKeydown: function(event) {
+                var movement = {
+                    "up": false,
+                    "dowm": false,
+                    "left": false,
+                    "right": false,
+                }
+                if (event.keyCode == 65) {
+                    movement.left = true;
+                }
+                if (event.keyCode == 68) {
+                    movement.right = true;
+                }
+                if (event.keyCode == 87) {
+                    movement.up = true;
+                }
+                if (event.keyCode == 83) {
+                    movement.down = true;
+                }
 
-    //draw
-    this.graphics.draw();
-  }
+                obj.player.move(movement);
+
+                this._pressed[event.keyCode] = true;
+            },
+            onKeyup: function(event) {
+                delete this._pressed[event.keyCode];
+            }
+        }
+    }
+
+    draw() {
+        //get all necessary data
+        this.graphics.visibleMap = this.map.build(this.player.position);
+
+        //draw
+        this.graphics.draw();
+    }
 }
 
 class Connection {
-  constructor(connString) {
-    this.socket = io(connString);
-  }
+    constructor(connString) {
+        this.socket = io(connString);
+    }
+
+    emit(data) {
+      this.socket.emit('boop');
+    }
 }
 
 class Draw {
-  constructor(canvas) {
-    this.c = document.getElementById(canvas);
-    this.ctx = this.c.getContext("2d");
+    constructor(canvas) {
+        this.c = document.getElementById(canvas);
+        this.ctx = this.c.getContext("2d");
 
-    this.visibleMap = {};
-  }
-
-  clearScreen() {
-    this.ctx.fillStyle="#f733e7";
-    this.ctx.fillRect(0, 0, this.c.width, this.c.height);
-  }
-
-  sidebar() {
-    this.ctx.fillStyle="#343434";
-    this.ctx.fillRect(1000, 0, 200, 800);
-  }
-
-  chat() {
-    this.ctx.fillStyle="#f4f142";
-    this.ctx.fillRect(0, 800, 1200, 200);
-  }
-
-  UI() {
-    this.sidebar();
-    this.chat();
-  }
-
-  getTile(tile) {
-    switch (tile) {
-      case "w1":
-        return "#ff0000";
-        break;
-      case "g1":
-        return "#33ff33";
-        break;
-      case "b":
-        return "#000";
-        break;
-      default:
-        return "#0000ff";
-        break;
+        this.visibleMap = {};
     }
-  }
 
-  visibleLand() {
-    // first numbers to centre the character
-    var xPos = -40 - (this.visibleMap.pModX),
-        yPos = -15 - (this.visibleMap.pModY);
-    for (var y = 1; y < this.visibleMap.map.length; y++) {
-      for (var x = 0; x < this.visibleMap.map[0].length; x++) {
-        var tile = this.visibleMap.map[y][x];
-        this.ctx.fillStyle = this.getTile(tile);
-        this.ctx.fillRect(xPos, yPos, 40, 40);
-        xPos += 40;
-      }
-      xPos = -40 - (this.visibleMap.pModX);
-      yPos += 40;
+    clearScreen() {
+        this.ctx.fillStyle = "#f733e7";
+        this.ctx.fillRect(0, 0, this.c.width, this.c.height);
     }
-  }
 
-  player() {
-    this.ctx.fillStyle="#ffff66";
-    this.ctx.fillRect(480, 355, 40, 70);
-  }
+    sidebar() {
+        this.ctx.fillStyle = "#343434";
+        this.ctx.fillRect(1000, 0, 200, 800);
+    }
 
-  draw() {
-    this.clearScreen();
-    this.visibleLand();
-    this.player();
-    this.UI();
-  }
+    chat() {
+        this.ctx.fillStyle = "#f4f142";
+        this.ctx.fillRect(0, 800, 1200, 200);
+    }
+
+    UI() {
+        this.sidebar();
+        this.chat();
+    }
+
+    getTile(tile) {
+        switch (tile) {
+            case "w1":
+                return "#ff0000";
+                break;
+            case "g1":
+                return "#33ff33";
+                break;
+            case "b":
+                return "#000";
+                break;
+            default:
+                return "#0000ff";
+                break;
+        }
+    }
+
+    visibleLand() {
+        // first numbers to centre the character
+        var xPos = -40 - (this.visibleMap.pModX),
+            yPos = -15 - (this.visibleMap.pModY);
+        for (var y = 1; y < this.visibleMap.map.length; y++) {
+            for (var x = 0; x < this.visibleMap.map[0].length; x++) {
+                var tile = this.visibleMap.map[y][x];
+                this.ctx.fillStyle = this.getTile(tile);
+                this.ctx.fillRect(xPos, yPos, 40, 40);
+                xPos += 40;
+            }
+            xPos = -40 - (this.visibleMap.pModX);
+            yPos += 40;
+        }
+    }
+
+    player() {
+        this.ctx.fillStyle = "#ffff66";
+        this.ctx.fillRect(480, 355, 40, 70);
+    }
+
+    draw() {
+        this.clearScreen();
+        this.visibleLand();
+        this.player();
+        this.UI();
+    }
 
 }
 
 class Player {
-  constructor(map) {
-    this.position = [40, 40];
-    this.map = map;
-  }
+    constructor(map) {
+        this.position = [40, 40];
+        this.map = map;
+    }
 
-  move(movement) {
-    this.position = this.map.checkMove(this.position, movement, 3);
-  }
+    move(movement) {
+        this.position = this.map.checkMove(this.position, movement, 3);
+    }
 }
 
 class Map {
-  constructor() {
-
-  }
-
-  checkMove(currPos, dir, speed) {
-    var userPosOnGrid = {
-      "x": Math.floor(currPos[0] / 40),
-      "y": Math.floor(currPos[1] / 40)
+    constructor() {
+        this.map = map;
     }
 
-    // var targetMove = {
-    //   "x": Math.floor((currPos[0]))
-    // }
-  }
-
-  build(playerPos) {
-    var rtnMap = [];
-    var userPosOnGrid = {
-      "x": Math.floor(playerPos[0] / 40),
-      "y": Math.floor(playerPos[1] / 40)
-    }
-    for (var y = userPosOnGrid.y - 11; y < userPosOnGrid.y + 13; y++) {
-      var row = [];
-      for (var x = userPosOnGrid.x - 13; x < userPosOnGrid.x + 14; x++) {
-        if (y < 0 || x < 0) {
-          row.push("b");
-        } else if (y >= map.sulran.ground.length || x >= map.sulran.ground[0].length) {
-          row.push("b");
-        } else {
-          row.push(map.sulran.ground[y][x]);
+    checkMove(currPos, dir, speed) {
+        var userPosOnGrid = {
+            "x": Math.floor(currPos[0] / 40),
+            "y": Math.floor(currPos[1] / 40)
         }
-      }
-      rtnMap.push(row);
-    }
-    var graphics = {
-      "map": rtnMap,
-      "pModX": playerPos[0] % 40,
-      "pModY": playerPos[1] % 40
+
+        var xMove = 0,
+            yMove = 0;
+
+        if (dir.up)
+            yMove -= speed;
+        if (dir.down)
+            yMove += speed;
+        if (dir.left)
+            xMove -= speed;
+        if (dir.right)
+            xMove += speed;
+
+        var targetPos = {
+            "x": Math.floor((currPos[0] + xMove) / 40),
+            "y": Math.floor((currPos[1] + yMove) / 40)
+        }
+
+        //check if left side hits anything                                        right side                                                   bottom
+        if (this.map.sulran.ground[targetPos.y][targetPos.x] != "w1" && this.map.sulran.ground[targetPos.y][targetPos.x + 1] != "w1"&& this.map.sulran.ground[targetPos.y + 1][targetPos.x] != "w1") {
+          return [currPos[0] + xMove, currPos[1] + yMove];
+        }
+        else
+            return [currPos[0], currPos[1]];
     }
 
-    return graphics;
-  }
+    build(playerPos) {
+        var rtnMap = [];
+        var userPosOnGrid = {
+            "x": Math.floor(playerPos[0] / 40),
+            "y": Math.floor(playerPos[1] / 40)
+        }
+        for (var y = userPosOnGrid.y - 11; y < userPosOnGrid.y + 13; y++) {
+            var row = [];
+            for (var x = userPosOnGrid.x - 13; x < userPosOnGrid.x + 14; x++) {
+                if (y < 0 || x < 0) {
+                    row.push("b");
+                } else if (y >= map.sulran.ground.length || x >= map.sulran.ground[0].length) {
+                    row.push("b");
+                } else {
+                    row.push(map.sulran.ground[y][x]);
+                }
+            }
+            rtnMap.push(row);
+        }
+        var graphics = {
+            "map": rtnMap,
+            "pModX": playerPos[0] % 40,
+            "pModY": playerPos[1] % 40
+        }
+
+        return graphics;
+    }
 
 
 }
