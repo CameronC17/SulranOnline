9b37e13ce545f1d1582aba017ab94137ba7a1c99
CameronC17 <cameronchalmers@hotmail.co.uk>
2016-11-14 21:08:27 +0000

    keyboard movement is nice

--- mapcreator/app.js
+++ mapcreator/app.js
@@ -9,6 +9,7 @@ var ctx = c.getContext("2d");
 
 var camera = { "x": 3, "y": 5 };
 var mouse = { "x": -1, "y": -1};
+var lastPress = new Date().getTime();
 
 window.addEventListener('mousedown', function(e) {Mouse.mouseDown()}, false);
 window.addEventListener('mouseup', function(e) {Mouse.mouseUp()}, false);
@@ -43,7 +44,7 @@ function getTile(tile) {
 function drawMap() {
   for (var y = 0; y < 30; y++) {
     for (var x = 0; x < 56; x++) {
-      if (x + camera.x < map.sulran.ground[0].length && y + camera.y < map.sulran.ground.length) {
+      if (x + camera.x >= 0 && y + camera.y >= 0 && x + camera.x < map.sulran.ground[0].length && y + camera.y < map.sulran.ground.length) {
         ctx.fillStyle=getTile(map.sulran.ground[y + camera.y][x + camera.x]);
         ctx.fillRect(x * 25, y * 25, 25, 25);
       } else {
@@ -51,6 +52,23 @@ function drawMap() {
         ctx.fillRect(x * 25, y * 25, 25, 25);
       }
     }
+  }
+}
+
+function checkMovementKeys() {
+  var now = new Date().getTime();
+  //if the last press was over 0.2secs ago
+  if (now > lastPress + 100) {
+    if (Key.isDown(Key.LEFT))
+      camera.x--;
+    if (Key.isDown(Key.RIGHT))
+      camera.x++;
+    if (Key.isDown(Key.UP))
+      camera.y--;
+    if (Key.isDown(Key.DOWN))
+      camera.y++;
+
+    lastPress = now;
   }
 }
 
@@ -63,6 +81,7 @@ var recursiveAnim = function() {
   drawMap();
   drawSidebar();
   drawMouseHover();
+  checkMovementKeys();
   animFrame(recursiveAnim);
 };
 animFrame(recursiveAnim);
@@ -81,14 +100,15 @@ var Mouse = {
       }
   },
   mouseDown: function() {
-    console.log(this.pos.x > 0, this.pos.y > 0, this.pos.x <= c.width, this.pos.y <= c.height)
-    if (this.pos.x > 0 && this.pos.y > 0 && this.pos.x <= c.width && this.pos.y <= c.height)
       this.down = true;
-    console.log(this.pos);
   },
   mouseUp: function() {
     this.pos = {"x": -1, "y": -1};
+    lastPress -= 500;
     this.down = false;
+  },
+  reset: function() {
+    this.pos = {"x": -1, "y": -1};
   }
 }
 
@@ -96,6 +116,10 @@ var Key = {
   //http://www.cambiaresearch.com/articles/15/javascript-char-codes-key-codes
   _pressed: {},
   SPACE: 32,
+  LEFT: 65,
+  UP: 87,
+  RIGHT: 68,
+  DOWN: 83,
   isDown: function(keyCode) {
       return this._pressed[keyCode];
   },
